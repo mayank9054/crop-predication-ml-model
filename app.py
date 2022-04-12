0d1034561ed02a15ab77a719f25c7cdabb7f93ff
Mayank Chhatraya <73630309+mayank9054@users.noreply.github.com>
2022-04-12 22:50:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+
+# Create flask app
+app = Flask(__name__)
+model = pickle.load(open("model_crop.pkl", "rb"))
+
+@app.route("/")
+def Home():
+    return "Welcome To Crop predection Model"
+
+@app.route("/predict", methods = ["POST"])
+def predict():
+    data = request.get_json(force=True) 
+    prediction = model.predict([[data['n'],data['p'],data['k'],data['t'],data['h'],data['ph'],data['r']]]) 
+    print(prediction)
+    output = prediction[0]
+    print(output)
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
