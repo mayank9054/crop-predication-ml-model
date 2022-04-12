94b2310599b41a5b7fe8122ef9cdbb28a69bbffb
Mayank Chhatraya <73630309+mayank9054@users.noreply.github.com>
2022-04-12 23:25:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,14 +11,18 @@ model = pickle.load(open("model_crop.pkl", "rb"))
 def Home():
     return "Welcome To Crop predection Model"
 
-@app.route("/predict", methods = ["POST"])
+@app.route("/predict", methods = ["POST","GET"])
 def predict():
-    data = request.get_json(force=True) 
-    prediction = model.predict([[data['n'],data['p'],data['k'],data['t'],data['h'],data['ph'],data['r']]]) 
-    print(prediction)
-    output = prediction[0]
-    print(output)
-    return jsonify(output)
-
+    if request.method == "POST":
+        data = request.get_json(force=True) 
+        prediction = model.predict([[data['n'],data['p'],data['k'],data['t'],data['h'],data['ph'],data['r']]]) 
+        print(prediction)
+        output = prediction[0]
+        print(output)
+        return jsonify(output)
+    else : 
+        return "predict"
+    
+ 
 if __name__ == "__main__":
     app.run(debug=True)
